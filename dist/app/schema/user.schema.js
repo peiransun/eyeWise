cd0262574989e836be5c312fd385b48d7ca13dca
peiran <peiran.sun@gmail.com>
2018-01-08 10:16:58 +0800

    同步處理

--- dist/app/schema/user.schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-exports.UserSchema = new mongoose_1.Schema({
-    name: { type: String },
-    email: { type: String }
-});
-//# sourceMappingURL=user.schema.js.map
--- dist/app/viewmodel/user.viewmodel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-class User extends mongoose_1.Document {
-}
-exports.default = User;
-//# sourceMappingURL=user.viewmodel.js.map
